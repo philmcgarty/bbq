2aefc27e108ac78a934c49aa12e25c984567bc81
Phil McGarty <philmcgarty@hotmail.com>
2022-04-06 20:08:15 -0400

    styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,12 @@ var recipeData = {};
 
 var ul= document.getElementById ('list-recipes');
 
+
+
+
+
+
+
 // basic function to pull bbq info from the recipe API
 var getRecipeData = function(){
     
@@ -39,9 +45,10 @@ var getRecipeData = function(){
 
 
 // FUNCTION TO CREATE P TAG AND ADD WEATHER ELEMENT
-var addWeatherData = function(dayElement, weatherDataType){
+var addWeatherData = function(dayElement, weatherDataType, classStyle){
     var paraElement = document.createElement("p");
     paraElement.innerHTML = weatherDataType;
+    paraElement.setAttribute("class", classStyle);
     dayElement.appendChild(paraElement);
 }
 
@@ -70,26 +77,30 @@ var useWeatherData = function(weatherData){
         dayElement.appendChild(weatherIconDisplay);
         // display description
         var weatherDescription = weatherData.daily[i].weather[0].description;
-        addWeatherData(dayElement, weatherDescription);
+        // var para = document.createElement("p");
+        // para.setAttribute("class", "capitalize");
+        // para.innerHTML = weatherDescription;
+        // dayElement.appendChild(para);
+        addWeatherData(dayElement, weatherDescription, "capitalize");
         // display day temp
         var dayTemp = Math.floor(weatherData.daily[i].temp.day);
-        var dayTempStr = `Day Temp: ${dayTemp} °C`;
+        var dayTempStr = `Day: ${dayTemp}`;
         addWeatherData(dayElement, dayTempStr);
         // display daytime feels like temp
         var dayTempFeelsLike = Math.floor(weatherData.daily[i].feels_like.day);
-        var dayFeelsStr = `Feels Like: ${dayTempFeelsLike} °C`;
+        var dayFeelsStr = `Feels: ${dayTempFeelsLike}`;
         addWeatherData(dayElement, dayFeelsStr);
         // display evening temp
         var eveTemp = Math.floor(weatherData.daily[i].temp.eve);
-        var eveTempStr = `Eve Temp: ${eveTemp} °C`;
+        var eveTempStr = `Eve: ${eveTemp}`;
         addWeatherData(dayElement, eveTempStr);
-        // display evening feels like temp
-        var eveTempFeelsLike = Math.floor(weatherData.daily[i].feels_like.eve);
-        var eveFeelsStr = `Feels Like: ${eveTempFeelsLike} °C`;
-        addWeatherData(dayElement, eveFeelsStr);
+        // display evening feels like temp - removed for space
+        // var eveTempFeelsLike = Math.floor(weatherData.daily[i].feels_like.eve);
+        // var eveFeelsStr = `Feels: ${eveTempFeelsLike}`;
+        // addWeatherData(dayElement, eveFeelsStr);
         // display UVI
         var uvi = Math.floor(weatherData.daily[i].uvi);
-        var uviStr = `UV Index: ${uvi}`;
+        var uviStr = `UVI: ${uvi}`;
         addWeatherData(dayElement,uviStr);
         // var for displaying weather analysis
         var comment = ""
@@ -97,16 +108,16 @@ var useWeatherData = function(weatherData){
         if (dayTemp>=20 && weatherData.daily[i].weather[0].id>=800){
             comment = "Perfect, fire up the BBQ!";
             dayElement.classList.add("good");
-        } else if (dayTemp>=15 && weatherData.daily[i].weather[0].id>=800) {
+        } else if (dayTemp>=10 && weatherData.daily[i].weather[0].id>=800) {
             comment = "Good conditions";
             dayElement.classList.add("moderate");
-        } else if (dayTemp<15) {
+        } else if (dayTemp<10) {
             comment = "Brrr a bit chilly!";
             dayElement.classList.add("cold");
         } else {
             comment = "Maybe not today!"
         };
-        addWeatherData(dayElement, comment);
+        addWeatherData(dayElement, comment, "bold");
     };
 }
 
